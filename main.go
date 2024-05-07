1437d4ab90433306d6061651d14922e9f825184f
RobertoJuniorTempesta <robertotempesta88@gmail.com>
2024-05-06 21:30:41 -0300

    feat: add logger

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"github.com/RobertoTempesta/goapi/config"
 	"github.com/RobertoTempesta/goapi/router"
 )
 
+var (
+	logger config.Logger
+)
+
 func main() {
-	// Initializae Router
+	//Initialize logger
+	logger = *config.GetLogger("main")
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+	// Initialize Router
 	// Obs: export é pela inicial do function sempre maiuscula
 	router.Initialize()
 }
